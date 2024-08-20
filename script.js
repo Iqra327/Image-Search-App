e7aa05a0f019f27aa7e5224ee3af8a65dac65b3a
Iqra327 <iqrarasheed219@gmail.com>
2024-08-20 22:28:10 +0500

    initial-commit

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
+const accessKey = '';
+const searchForm = document.querySelector('form');
 const searchInput = document.querySelector('.js-search-input');
+const searchIcon = document.querySelector('.js-search-icon');
 const imagesContainer = document.querySelector('.js-images-container');
+
+searchForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const inputValue = searchInput.value.trim();
+  fetchImages(inputValue);
+});
+
+const url = `https://api.unsplash.com/photos/?client_id=YOUR_ACCESS_KEY`
+function fetchImages(inputValue){
+ console.log('helo');
+}

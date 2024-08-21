0384b92ebf27a55c12d757f0c07a3862137d2f96
Iqra327 <iqrarasheed219@gmail.com>
2024-08-21 17:39:13 +0500

    in-process

--- script.js
+++ script.js
@@ -1,16 +1,48 @@
-const accessKey = '';
+const encodedKey = 'ZlZ5Z1BoMmlNOUVMMDZOWkJsUjhRa0Z1dXAzQkJWUzAtQ3VtbGJETk5IRQ==';
+const accessKey = atob(encodedKey);
+
 const searchForm = document.querySelector('form');
 const searchInput = document.querySelector('.js-search-input');
 const searchIcon = document.querySelector('.js-search-icon');
 const imagesContainer = document.querySelector('.js-images-container');
+const heading = document.querySelector('.js-heading');
+
+async function fetchImages(inputValue){
+  const url = `https://api.unsplash.com/search/photos?query=${inputValue}&per_page=28&client_id=${accessKey}`;
+
+  try {
+    if(inputValue){
+      heading.innerHTML = 'Wait for the images to load...';
+      imagesContainer.innerHTML = '';
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+      
+      data.results.forEach(photo=> {
+        const imageElement = document.createElement('div');
+        imageElement.classList.add('imageDiv');
+        imageElement.innerHTML = `<img src="${photo.urls.regular}" alt="img">`;
+
+        const imageOverlay = document.createElement('div');
+        imageOverlay.classList.add('overlay');
+
+        imagesContainer.appendChild(imageElement);
+        imageElement.appendChild(imageOverlay);
+        heading.innerHTML = '';
+      });
+    }
+    else{
+      heading.innerHTML = 'Type the name of the image category...';
+      imagesContainer.innerHTML = '';
+    }
+  
+  } catch (error) {
+    heading.innerHTML = 'Error happened!! Try again later..'   
+  }
+};
 
 searchForm.addEventListener('submit', (e) => {
   e.preventDefault();
   const inputValue = searchInput.value.trim();
-  fetchImages(inputValue);
+    fetchImages(inputValue);
 });
-
-const url = `https://api.unsplash.com/photos/?client_id=YOUR_ACCESS_KEY`
-function fetchImages(inputValue){
- console.log('helo');
-}
